bd220c3f4b7143abbc71c47239ff657855d298b1
Khoi-Hoan <68211832+Khoi-Hoan@users.noreply.github.com>
2021-07-28 19:44:34 +0700

    read file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
-import java.util.Scanner;
+import java.net.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.print("Hello world");
-        System.out.print("1_2_3_4");
+    public static void main(String[] args) {
+
+    }
+    public static void readFile() throws IOException {
+        URL url = new URL("https://raw.githubusercontent.com/TriDang/cosc2081/main/assignments/project/covid-data.csv");
+        BufferedReader read = new BufferedReader(
+                new InputStreamReader(url.openStream()));
+        String i;
+        while ((i = read.readLine()) != null)
+            System.out.println(i);
+        read.close();
     }
 }
+
+
+
+/*public class data {
+    private int iso_code;
+    private String continent;
+    private String location;
+    private
+}*/
