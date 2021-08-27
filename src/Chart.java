aaabd89bbcefc527d1827c7532963231bc83ab12
Khoi-Hoan <68211832+Khoi-Hoan@users.noreply.github.com>
2021-08-28 04:52:03 +0700

    chart sort of

---
+++ src/Chart.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+public class Chart {
+    public static void main(String[] args) {
+        ArrayList<Integer> i = new ArrayList<>();
+        i.add(552435);
+        i.add(4654);
+        i.add(176582);
+        i.add(17460);
+        i.add(18675);
+        chartDisplay(chart(i));
+    }
+    public static String[][] chart(ArrayList<Integer> values){
+        String[][] chart = new String[24][80];
+        for (int j = 0; j < 24; j++){
+            for (int i = 0; i < 80; i++){
+                if (j == 0){
+                    chart[j][i] = "_";
+                }
+                else if (i == 0){
+                    chart[j][i]= "|";
+                }
+                else {
+                    chart[j][i]= "";
+                }
+            }
+        }
+
+        int noCollum = values.size();
+        System.out.println(noCollum);
+        int xDistance = 80 / noCollum;
+        System.out.println(xDistance);
+        int minValue = values.get(minIndex(values));
+        System.out.println(minValue);
+        int maxValue = values.get(maxIndex(values));
+        System.out.println(maxValue);
+        int yDistance = (maxValue - minValue) /24;
+        if (yDistance < 1){
+            yDistance = 1;
+        }
+        System.out.println(yDistance);
+        int xPos = xDistance / 2;
+        int recentValue;
+        for (int value = 0; value < values.size(); value++){
+            recentValue = minValue;
+            for (int yPos = 0; yPos < 24; yPos++) {
+                if ( xPos < 80) {
+                    if (recentValue <= values.get(value)){
+                        chart[yPos][xPos] = "*";
+                        recentValue += xDistance;
+                    }
+                }
+            }
+            xPos += xDistance;
+        }
+        return chart;
+    }
+
+    public static void chartDisplay(String[][] chart){
+        for (int j = 23; j > -1; j--){
+            for (int i = 0; i < 80; i++){
+                System.out.print(chart[j][i]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int minIndex(ArrayList<Integer> list) {
+        int index = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) < list.get(index)) {
+                index = i;
+            }
+        }
+        return index;
+    }
+    public static int maxIndex(ArrayList<Integer> list) {
+        int index = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > list.get(index)) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
