32aebef9fe22adbaeb548cf9a76ef3864877a024
dinh ba dat <datdinh0709@gmail.com>
2021-08-29 23:41:21 +0700

    fixing tabular display

--- src/tabular.java
+++ src/tabular.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class tabular {
     public static void main(String[] args) {
-        Object[][] table = new String[17][];
+        String[][] table = new String[17][];
         table[0] = new String[]{"2", "5", "123522"};
         table[1] = new String[]{"5312132", "425345", "1535254"};
         table[2] = new String[]{"434983", "123535", "474453"};
@@ -20,18 +20,21 @@ public class tabular {
         table[14] = new String[]{"5312132", "425345", "1535254"};
         table[15] = new String[]{"434983", "1235435", "4744453"};
         table[16] = new String[]{"213454534", "5232323", "2"};
+        tabularDisplay(table);
+    }
         // create table form
+        public static void tabularDisplay(String[][] tabular){
         System.out.println("-----------------------------------------------------------");
         System.out.format("%c %21s %13c %10s %10c\n",'|', "Range(date 1- date 2)", '|', "Value",'|');
         System.out.println("|---------------------------------------------------------|");
         // insert data into table
-        for (int i = 0; i < table.length; i++) {
-            Object[] row = table[i];
+        for (int i = 0; i < tabular.length; i++) {
+            Object[] row = tabular[i];
             System.out.format("%c %9s %4c %9s %10c %9s %11c\n",'|', row[0],'-', row[1], '|', row[2],'|');
         }
         System.out.println("-----------------------------------------------------------");
     }
-}
 
+}
 
 
