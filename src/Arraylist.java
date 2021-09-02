a3ddcdbd086b1901f1294278911ba070e4ac0b03
Jujinho <68211902+Jujinho@users.noreply.github.com>
2021-09-03 01:26:35 +0900

    Create Arraylist.java
    help me

---
+++ src/Arraylist.java
@@ -0,0 +1,31 @@
+import java.util.LinkedHashMap;
+
+public class Arraylistdisplay {
+  public static void main(String[] args) {
+    
+    public class Arraylist1 {
+    Data strdisplay = new TabularDIsplay();
+    strdisplay.addDataPoint(String: "AFG");
+    display.strdisplay();
+    }
+    public class Arraylist2 {
+   
+    Data intdisplay = new TabularDIsplay();
+    intdisplay.addDataPoint(interger:38928341);
+    display.intdisplay();
+  }
+
+  
+
+  public class Tabular {
+    DataDisplay display = new TabularDisplay();
+    display.addDataPoint();
+    display.display();
+
+  }
+
+
+
+
+
+}
