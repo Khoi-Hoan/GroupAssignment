29f8f05f4fa161ab6be09b85be2d3af3d5bb662a
Jujinho <68211902+Jujinho@users.noreply.github.com>
2021-09-03 01:17:24 +0900

    Create Arraylist.java

---
+++ src/Arraylist.java
@@ -0,0 +1,27 @@
+import java.util.LinkedHashMap;
+
+public class Arraylist {
+  public static void main(String[] args) {
+    Data intdisplay = new TabularDIsplay();
+    intdisplay.addDataPoint(interger:38928341);
+    display.intdisplay();
+  }
+    
+  public class Arraylist2 {
+    Data strdisplay = new TabularDIsplay();
+    strdisplay.addDataPoint(String: "AFG");
+    display.strdisplay();
+    }
+  
+  public class Tabular {
+    DataDisplay display = new TabularDisplay();
+    display.addDataPoint();
+    display.display();
+  
+  }
+  
+    
+    
+    
+  
+}
